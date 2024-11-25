a9c67258423e7a429502be88658fb7acdc2dab28
Manoj <manojbharathi00@gmail.com>
2024-11-25 10:21:21 +0530

    date issues fixed

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+import moment from "moment";
 import { dbGetDatabase, dbPushDataBase, getConnect } from "./db.connection.js";
 
-const getQuery = `select  TO_CHAR(attdate, 'dd-mm-yyyy') as attdate, chk, compcode, 
+const getQuery = `select  attdate, chk, compcode, 
 dtype, hremploymastid, hrmattimportid, idcard, inout,ipadd,pmno,unqid,username  from FKATT where rowNum <= 2`;
 
 const deleteQuery = `delete from fkatt`
 
-const formatValue = (value, index) => {
-    if (index == 0) return `TO_DATE('${value}','DD-MM-YYYY')`
+const formatValue = (value, index, metaData) => {
+
+
+    if (metaData[index]?.dbTypeName == "DATE") {
+        // return `'${new Date(value)}'`
+        return `TO_DATE('${moment(value).format("DD/MM/YYYY")}', 'DD/MM/YYYY')`
+        // return `TO_DATE(CURRENT_DATE, 'DD/MM/YY')`
+    }
     return `'${value}'`
 }
 
 const constructPushQuery = (res) => `
 INSERT All   
 ${res.rows.map(row => `INTO FKATT (${res.metaData.map(i => i?.name).join(",")}) VALUES (${row.map((value, index) =>
-    formatValue(value, index)
+    formatValue(value, index, res.metaData)
 ).join(',')}) `).join(" ")} SELECT * FROM DUAL`;
+
 async function getRes(connection, sqlQuery) {
     try {
         const result = await connection.execute(sqlQuery)
@@ -30,8 +38,12 @@ async function main() {
     try {
         connection1 = await getConnect(dbGetDatabase)
         let res = await getRes(connection1, getQuery);
+        console.log(res, 'data');
+
         connection2 = await getConnect(dbPushDataBase)
         let pushQuery = constructPushQuery(res);
+        console.log(pushQuery, 'ins');
+
         await getRes(connection2, deleteQuery);
         await getRes(connection2, pushQuery);
         connection2.commit();
